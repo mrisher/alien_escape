f50ca7419fc1c9ffad4ad39e92a5eb3a708a9232
Mark Risher <218038+mrisher@users.noreply.github.com>
2022-07-20 21:27:58 -0700

    still broken

--- src/matrixPulsarFsm.cpp
+++ src/matrixPulsarFsm.cpp
@@ -9,8 +9,8 @@ void MatrixPulsarFSM::Setup() {
   this->AddState(matrixPulsarStateName[PULSAR_OFF], BLINK_TIME, nullptr, nullptr, nullptr);
   // Add transitions with related trigger input callback functions
   // In this example it's just a simple lambda function that return state timeout value
-  AddTransition(PULSAR_ON, PULSAR_OFF, [&]()
-                                { return this->CurrentState()->timeout; });
+  AddTransition(PULSAR_ON, PULSAR_OFF, []()
+                                { return YA_FSM::CurrentState()->timeout; });
   this->AddTransition(PULSAR_OFF, PULSAR_ON, [&]()
                                 { return CurrentState()->timeout; });
   this->AddAction(PULSAR_ON, YA_FSM::S, matrixPulsarLedState);    // S = set to on
